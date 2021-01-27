459828d0e1d174b5fd3fb65b0c8bf1a7084e8e52
Alexander Nikolaev <nikolaev.warrior@gmail.com>
2021-01-27 22:32:36 +0500

    Setup constraints

--- HW2.1 Ex.2/ViewController.swift
+++ HW2.1 Ex.2/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redView.layer.cornerRadius = 60
+        redView.alpha = 0.3
+        yellowView.layer.cornerRadius = 60
+        yellowView.alpha = 0.3
+        greenView.layer.cornerRadius = 60
+        greenView.alpha = 0.3
     }
 
-
+    @IBAction func startButtonTapped() {
+    }
+    
 }
 
